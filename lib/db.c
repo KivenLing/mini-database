a45301a8499c3d4931d1c577490cf77b2bf9a397
KivenLing <KivenL96@163.com>
2019-11-25 15:38:50 +0800

    db_close() done

--- lib/db.c
+++ lib/db.c
@@ -33,22 +33,21 @@
 #define IDX_FILE_FLAG (O_RDWR | O_CREAT)
 #define DAT_FILE_FLAG (O_RDWR | O_CREAT | O_APPEND)
 
-
 struct DB
 {
     int datfd;
     int idxfd;
     off_t cur_off;
-    char* name;
-    struct bplus_tree* tree;
+    char *name;
+    struct bplus_tree *tree;
 };
 
 /*
  * 初始化DB结构体
  */
-static struct DB* _db_alloc(int namelen)
+static struct DB *_db_alloc(int namelen)
 {
-    struct  DB *db = calloc(1, sizeof(struct DB));
+    struct DB *db = calloc(1, sizeof(struct DB));
     if (db = NULL)
     {
         err_dump("_db_alloc: calloc error");
@@ -59,13 +58,14 @@ static struct DB* _db_alloc(int namelen)
     {
         err_dump("_db_alloc: malloc error");
     }
+    db->tree = bplus_tree_init(BPLUS_MAX_ORDER, BPLUS_MAX_ENTRIES);
     return db;
 }
 
 /*
  * 释放DB资源
  */
-static void _db_free(struct DB* db)
+static void _db_free(struct DB *db)
 {
     if (db)
     {
@@ -84,18 +84,17 @@ static void _db_free(struct DB* db)
         }
         free(db);
     }
-    
 }
 
 /*
  * 将buf[start, start + len - 1]字符复制
  */
-static char* _buf_cpy(char* buf, int start, int len)
+static char *_buf_cpy(char *buf, int start, int len)
 {
     /* 保证buf复制不越界 */
     assert(strlen(buf) > start + len);
 
-    char* cpystr = (char*)malloc(len + 1);
+    char *cpystr = (char *)malloc(len + 1);
     int i;
     for (i = 0; i < len; i++)
     {
@@ -108,28 +107,51 @@ static char* _buf_cpy(char* buf, int start, int len)
 /*
  * 将buf[start, start + len - 1]字符转换成数字
  */
-int _buf_to_int(char* buf, int start, int len)
+int _buf_to_int(char *buf, int start, int len)
 {
-    char* dest_str = _buf_cpy(buf, start, len);
+    char *dest_str = _buf_cpy(buf, start, len);
     int res = atoi(dest_str);
     free(dest_str);
     return res;
 }
 
 /*
- * 
+ * 将整型转换成字符串，填充buf[start, start + len - 1]，填充fill字符
  */
-
-char* _int_to_buf()
+static void _int_to_buf(char *buf, int num, int start, int len, char fill)
 {
-
+    assert(num >= 0);
+    int i = -1;
+    char stack[len + 1];
+    int temp;
+    do
+    {
+        i++;
+        temp = num % 10;
+        stack[i] = '0' + temp;
+        num /= 10;
+    } while (num > 0 && i <= len + 1);
+    int j = 0;
+    /* 填充数字字符 */
+    while (i >= 0 && j < len)
+    {
+        buf[start + j] = stack[i];
+        j++;
+        i--;
+    }
+    /* 填充fill字符 */
+    while (j < len)
+    {
+        buf[start + j] = fill;
+        j++;
+    }
 }
 
 /*
  * 将索引文件解析，转换成B+树
  * 一次读取4096字符进行解析
 */
-static void _file_to_tree(struct DB* db)
+static void _file_to_tree(struct DB *db)
 {
     char buf[MAXLINE];
     int idx_in_block = -1;
@@ -141,12 +163,57 @@ static void _file_to_tree(struct DB* db)
         kv_num = readlen / IDX_DAT_LEN;
         for (idx_in_block = 0; idx_in_block <= kv_num; idx_in_block++)
         {
-            
+            key_t key = _buf_to_int(buf, IDX_DAT_LEN * idx_in_block, IDX_LEN);
+            /* 第 9 - 30 是记录off */
+            int value = _buf_to_int(buf, IDX_DAT_LEN * idx_in_block + IDX_LEN + 1, IDX_DAT_LEN - IDX_LEN - 2);
+            bplus_tree_put(db->tree, key, value);
         }
-        
+        block_num++;
     }
-    
+}
 
+/*
+ * 将叶子节点写入索引文件
+ */
+static int _tree_leaf_to_file(struct DB* db, struct bplus_leaf* leaf)
+{
+    if (leaf == NULL)
+        return;
+    int count = leaf->entries;
+    char buf[IDX_DAT_LEN * count];
+    for (int i = 0; i < count; i++)
+    {
+        int k = leaf->key[i];
+        int v = leaf->data[i];
+        _int_to_buf(buf, k, IDX_DAT_LEN * i, IDX_LEN, SAPCE);
+        buf[IDX_DAT_LEN * i + IDX_LEN] = SEP;
+        _int_to_buf(buf, IDX_DAT_LEN * i + IDX_LEN + 1, IDX_DAT_LEN - IDX_LEN - 2, SAPCE);
+        buf[IDX_DAT_LEN * (i + 1) - 1] = NEWLINE;
+    }
+    if (write(db->idxfd, buf, IDX_DAT_LEN * count) < 0)
+    {
+        err_dump("_tree_leaf_to_file: write error");
+        return -1;
+    }
+    return 1;
+}
+
+/*
+ * 将B+树写入索引文件
+ * 1. 遍历B+树，保存k, v
+ * 2. 将k, v转换字符串，写入索引文件
+ * 问题：这里用的方法效率较低, 原因是每次写入量小(主要)，单线程
+ */
+static void _tree_to_file(struct DB* db)
+{
+    struct bplus_leaf* first = first_leaf_node(db->tree);
+    struct bplus_leaf* last = last_leaf_node(db->tree);
+    while (first != last)
+    {
+        _tree_leaf_to_file(db, first);
+        first = next_leaf_node(first);
+    }
+    _tree_leaf_to_file(db, last);
 }
 
 /*
@@ -157,7 +224,7 @@ static void _file_to_tree(struct DB* db)
  */
 DBHANDLE db_open()
 {
-    struct DB* db;
+    struct DB *db;
     char dat_file[DB_NAME_MAX_LEN + 5];
     char idx_file[DB_NAME_MAX_LEN + 5];
     int namelen = strlen(DB_NAME);
@@ -176,5 +243,18 @@ DBHANDLE db_open()
         return NULL;
     }
     db->cur_off = lseek(db->datfd, 0, SEEK_END);
-    
+    _file_to_tree(db);
+    return (DBHANDLE)db;
+}
+
+/*
+ * 关闭数据库
+ * 1. 将B+树写入索引文件
+ * 2. 释放相关内存
+ */
+void db_close(DBHANDLE _db)
+{
+    struct DB *db = (struct DB *)_db;
+    _tree_to_file(db);
+    _db_free(db);
 }
